fa90fdfbb985c74e97199de1d351fe482ec82109
reneeteng <crystaltoffee@hotmail.com>
2021-06-09 11:36:24 +0800

    * Shorten intro words.

--- module/automation/lang/en.php
+++ module/automation/lang/en.php
@@ -12,24 +12,24 @@
 $lang->automation->common = 'Automated Testing';
 $lang->automation->browse = 'Home';
 
-$lang->automation->title = 'Test Automation Solutions';
+$lang->automation->title = 'ZenTao Test Automation Solution';
 
 $lang->automation->ztfSite     = 'ZTF Website';
 $lang->automation->ztfDownload = 'ZTF Download';
 $lang->automation->ztfManual   = 'ZTF Manual';
 
-$lang->automation->ztfFeature1 = 'ZTF solves problems for you.';
+$lang->automation->ztfFeature1 = 'ZTF solves testing problems';
 $lang->automation->ztfFeature2 = 'ZTF solves the following problems for you:';
-$lang->automation->ztfFeature3 = 'Management of use cases';
-$lang->automation->ztfFeature4 = 'Comparison of test results';
-$lang->automation->ztfFeature5 = 'Execution of test scripts';
-$lang->automation->ztfFeature6 = 'Bug reporting';
+$lang->automation->ztfFeature3 = 'test cases';
+$lang->automation->ztfFeature4 = 'test results';
+$lang->automation->ztfFeature5 = 'test scripts';
+$lang->automation->ztfFeature6 = 'bug reporting';
 
-$lang->automation->ztfFeature7  = 'ZTF is not depended on the environment';
-$lang->automation->ztfFeature8  = 'Written in Go';
-$lang->automation->ztfFeature9  = 'Deploy with no dependencies';
-$lang->automation->ztfFeature10 = 'Works on all platforms';
-$lang->automation->ztfFeature11 = 'Has only one executable';
+$lang->automation->ztfFeature7  = 'ZTF is environmental independent';
+$lang->automation->ztfFeature8  = 'written in Go';
+$lang->automation->ztfFeature9  = 'no dependencies';
+$lang->automation->ztfFeature10 = 'for all platforms';
+$lang->automation->ztfFeature11 = 'only one executable';
 
 $lang->automation->ztfFeature12 = 'ZTF is for all automated testing types';
 $lang->automation->ztfFeature13 = 'Support for common development languages';
@@ -46,19 +46,19 @@ $lang->automation->zendataDownload = 'ZenData Download';
 $lang->automation->zendataManual   = 'ZenData Manual';
 
 $lang->automation->zendata         = 'A generic data generation tool';
-$lang->automation->zendataFeature1 = 'Easy to get started with no coding knowledge required';
+$lang->automation->zendataFeature1 = 'Easy to start with no coding knowledge required';
 $lang->automation->zendataFeature2 = 'Customizable data format with no restrictions';
 $lang->automation->zendataFeature3 = 'Generate a million pieces of data with one click';
 
 $lang->automation->zendataFeature4 = 'Cross-platform and cross-language';
 $lang->automation->zendataFeature5 = 'Only one executable is required to run it';
-$lang->automation->zendataFeature6 = 'For Windows, Linux, and MacOS';
+$lang->automation->zendataFeature6 = 'For Windows, Linux, and macOS';
 $lang->automation->zendataFeature7 = 'For various languages';
 
-$lang->automation->zendataFeature8  = 'All kinds of black tricks';
+$lang->automation->zendataFeature8  = 'All kinds of techniques';
 $lang->automation->zendataFeature9  = 'Interval, Step Size, Random, Repeat, and Reference';
 $lang->automation->zendataFeature10 = 'Specify a list of files to query in Excel';
-$lang->automation->zendataFeature11 = 'Parallel generation, recursive generation, and reverse parsing';
+$lang->automation->zendataFeature11 = 'Parallel/recursive generation, and reverse parsing';
 
 $lang->automation->groupTips        = 'Welcome to the automated testing QQ group';
 $lang->automation->groupTitle       = 'Software testing QQ group:';
